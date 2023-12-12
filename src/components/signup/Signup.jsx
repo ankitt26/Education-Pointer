ff7d591b7fa11756d55cfadab16e896b23bac15b
Ankit <ankit26kushwa@gmail.com>
2023-12-13 02:48:27 +0530

    Fix Logo path typo

--- src/components/signup/Signup.jsx
+++ src/components/signup/Signup.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Logo from "..//../assets/images/Logo.png";
+import Logo from "../../assets/images/Logo.png";
 import { useState } from "react";
 import { Link } from "react-router-dom";
 
